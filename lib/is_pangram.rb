a127cbf67ac05949c3b5b78442aa17bdc017deb5
Lily <lilyblake@outlook.com>
2020-03-17 16:18:50 +0000

    added is_pangram?(x) method

--- lib/is_pangram.rb
+++ lib/is_pangram.rb
@@ -1,6 +1,13 @@
 require "is_pangram/version"
 
-module IsPangram
-  class Error < StandardError; end
-  # Your code goes here...
+def is_pangram?(x)
+  if x.instance_of?(String)== false
+    raise ArgumentError, "the argument must be a string"
+  end
+  if x.length < 26
+    false
+  else
+    x.downcase
+    x.count("a-z") > 0
+  end
 end
